42c10dfeacbcaabbb4b44bb3448a37d78073877c
lostleaf <lostleaf@me.com>
2016-02-07 01:30:08 -0600

    transform raw data to pickle file to acclerate data reading

---
+++ transform.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from utils import read_probes, read_links
+import cPickle
+
+def main():
+    probe_file = 'probe_points.csv'
+    link_file = 'links.csv'
+    probes = read_probes(probe_file)
+    links = read_links(link_file)
+    with open("probes.pkl", "wb") as fout:
+        cPickle.dump(probes, fout, 2)
+    with open("links.pkl", "wb") as fout:
+        cPickle.dump(links, fout, 2)
+
+if __name__ == "__main__":
+    main()
