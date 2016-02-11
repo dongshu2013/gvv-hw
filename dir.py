8ff12c0df8705d3f31cbed5d5ed4f7b0d4d18b65
lostleaf <lostleaf@me.com>
2016-02-10 20:17:02 -0600

    fix bug of direction estimation

--- dir.py
+++ dir.py
@@ -17,8 +17,17 @@ def calc_road_vec(probe, link_points):
 
     link_dis = calc_link_dis(base_side, side1, side2)
     min_idx = np.argmin(link_dis)
-    road_vec = np.array(link_points[min_idx + 1]) - np.array([min_idx])
+    road_vec = np.array(link_points[min_idx + 1]) - np.array(link_points[min_idx])
     return road_vec
+
+#drive from probe1 to probe2
+def get_cosine(probe1, probe2, road_vec):
+    drive_vec  =  np.array(probe2) - np.array(probe1)
+    road_vec /= np.linalg.norm(road_vec)
+    drive_vec /= np.linalg.norm(drive_vec)
+    # print probe1, probe2, drive_vec
+    cosine = np.dot(road_vec, drive_vec)
+    return cosine
 
 def main():
     probes, links = read_data()
@@ -26,41 +35,30 @@ def main():
         nearest_link = cPickle.load(fin)
     with open("data/probe_ids.pkl", "rb") as fin:
         probe_ids = cPickle.load(fin)
-    with open("data/probe_times.pkl", "rb") as fin:
-        probe_times = cPickle.load(fin)
-    with open("data/link_ids.pkl", "rb") as fin:
-        link_ids = cPickle.load(fin)
-    with open("/Users/lostleaf/dev/gvv-hw/data/dis_ref_nonref.pkl", "rb") as fin:
-        dis_ref_nonref = cPickle.load(fin)
-    dirs = []
-    with open("data/probe_points.csv", "r") as fin:
-        with open("data/Partition6467MatchedPoints.csv", "w") as fout:
-            for idx, (line, probe, nearest, probe_id, probe_time) in enumerate(izip(fin, probes, nearest_link, probe_ids, probe_times)):
-                if nearest is not None:
-                    nearest_idx, nearest_dis = nearest
-                    road_vec = calc_road_vec(probe, links[nearest_idx])
-                    road_vec /= np.linalg.norm(road_vec)
-                    sine = None
-                    if idx > 0 and probe_ids[idx - 1] == probe_id:
-                       drive_vec  =  np.array(probe) - np.array(probes[idx - 1])
-                       drive_vec /= np.linalg.norm(drive_vec)
-                       sine = road_vec[0] * drive_vec[1] - road_vec[1] * drive_vec[0]
-                    if idx < len(probe) - 1 and probe_ids[idx + 1] == probe_id:
-                       drive_vec  =  np.array(probes[idx + 1]) - np.array(probe)
-                       drive_vec /= np.linalg.norm(drive_vec)
-                       sine1 = road_vec[0] * drive_vec[1] - road_vec[1] * drive_vec[0]
-                       if sine is None or np.abs(sine1) > np.abs(sine):
-                           sine = sine1
-                    if sine:
-                        dir = sine > 0
-                    else:
-                        dir = True
-                    dis_ref, dis_nonref = dis_ref_nonref[idx]
-                    dir_chr = "F" if dir else "T"
-                    # print line + "," + ",".join((link_ids[nearest_idx], dir_chr, str(dis_ref), str(dis_nonref)))
-                    fout.write(line.strip() + "," + ",".join((link_ids[nearest_idx], dir_chr, str(dis_ref), str(dis_nonref))) + "\n")
-                if idx % 10000 == 0:
-                    print idx
+    dir_angles = []
+    for idx, (probe, nearest, probe_id) in enumerate(izip(probes, nearest_link, probe_ids)):
+        # print "probe", idx
+        if nearest is not None:
+            nearest_idx, nearest_dis = nearest
+            road_vec = calc_road_vec(probe, links[nearest_idx])
+            # print road_vec
+            cosine = None
+            if idx > 0 and probe_ids[idx - 1] == probe_id:
+                cosine = get_cosine(probes[idx - 1], probe, road_vec)
 
+            if idx < len(probe) - 1 and probe_ids[idx + 1] == probe_id:
+                cosine1 = get_cosine(probe, probes[idx + 1], road_vec)
+                if cosine is None or np.abs(cosine1) > np.abs(cosine):
+                    cosine = cosine1
+            if cosine is None:
+                cosine = 1
+            dir_angles.append(cosine)
+        else:
+            dir_angles.append(None)
+        if idx % 10000 == 0:
+            print idx
+    print len(dir_angles)
+    with open("data/dir_angles.pkl", "wb") as fout:
+        cPickle.dump(dir_angles, fout, 2)
 if __name__ == "__main__":
     main()
