101a8c887a6cd95b90ce0abc6a67841ac7861290
dongshu <dongs2011@gmail.com>
2016-02-19 23:46:26 -0600

    merge centers and tile_transform

--- hw5/centers.py
+++ hw5/centers.py
@@ -1,5 +1,51 @@
-from tile_transform import latlon_to_pix, pix_to_latlon
 from itertools import product
+import math
+
+MIN_LAT = -85.05112878;
+MAX_LAT = 85.05112878;
+MIN_LON = -180;
+MAX_LON = 180;
+
+
+def clip(n, min_value, max_value):
+    return min(max(n, min_value), max_value);
+
+
+def pix_to_latlon(px, py, level=20):
+    px = float(px)
+    py = float(py)
+    msize = 256 << level
+    x = (clip(px, 0, msize - 1) / msize) - 0.5;
+    y = 0.5 - (clip(py, 0, msize - 1) / msize);
+
+    lat = 90 - 360 * math.atan(math.exp(-y * 2 * math.pi)) / math.pi;
+    lon = 360 * x;
+    return (lat, lon)
+
+
+def latlon_to_pix(lat, lon, level = 20):
+    lat = clip(lat, MIN_LAT, MAX_LAT);
+    lon = clip(lon, MIN_LON, MAX_LON);
+
+    x = (lon + 180) / 360;
+    sin_lat = math.sin(lat * math.pi / 180);
+    y = 0.5 - math.log((1 + sin_lat) / (1 - sin_lat)) / (4 * math.pi);
+
+    msize = 256 << level
+    px = clip(x * msize + 0.5, 0, msize - 1);
+    py = clip(y * msize + 0.5, 0, msize - 1);
+    return (int(px), int(py))
+
+
+def test():
+    print (47.610, -122.107)
+    print latlon_to_pix(47.610, -122.107)
+
+    print '******************************'
+
+    print (43168150, 93745004)
+    print pix_to_latlon(43168150, 93745004)
+
 
 def get_centers(lat1, lon1, lat2, lon2, size=(512, 512)):
     lat1, lat2 = min(lat1, lat2), max(lat1, lat2)
@@ -14,6 +60,7 @@ def get_centers(lat1, lon1, lat2, lon2, size=(512, 512)):
     return [pix_to_latlon(x, y) for x, y in xy_pairs]
 
 def main():
+    test()
     print len(get_centers(47.678559869527817, -122.13099449872971, 47.688559869527817, -122.12099449872971))
 
 if __name__ == "__main__":
